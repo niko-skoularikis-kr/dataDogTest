9fa354a0131613a8e3e9b16c204a3e1e3897d380
niko-skoularikis-kr <niko.skoularikis@kroger.com>
2023-07-06 09:27:34 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+/**
+ * Send logs returns "Request accepted for processing (always 202 empty JSON)." response
+ */
+
+const { client, v2 } = require("@datadog/datadog-api-client")
+
+process.env['DD_SITE'] = "us3.datadoghq.com"
+ const configuration = client.createConfiguration();
+ const apiInstance = new v2.LogsApi(configuration);
+
+ const message = {
+  appName: "test",
+  environment: "test",
+  context: "test",
+  capabilityCount: 23423,
+  capabilityKeys: ["test", "two"],
+  capabilityError: "test",
+  capabilityName: "test",
+  capabilityVersions: ["test", "three"],
+  factoryVersion: "test",
+}
+
+
+ const params = {
+   body: [
+     {
+       ddsource: "nginx",
+       ddtags: "application-name:kap_web_client,version:5.1",
+       hostname: "i-012345678",
+       message: `hello ${JSON.stringify(message)}`,
+       service: "payment",
+       additionalProperties: {
+         status: "info",
+       },
+     },
+   ],
+ };
+ 
+ apiInstance
+   .submitLog(params)
+   .then((data) => {
+     console.log(
+       "API called successfully. Returned data: " + JSON.stringify(data)
+     );
+   })
+   .catch((error) => console.error(error));
